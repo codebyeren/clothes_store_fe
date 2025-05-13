35010e7a92b63f71851a5c991d32c3b751030f87
Eren <www.viet712@gmail.com>
2025-05-13 10:59:44 +0700

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,51 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home-page/home-page.component').then(m => m.HomePageComponent)
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent)
+      },
+      {
+        path: 'forget-password',
+        loadComponent: () => import('./auth/forget-password/forget-password.component').then(m => m.ForgetPasswordComponent)
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'products/:categoryId',
+    loadComponent: () => import('./product-category/product-category.component').then(m => m.ProductCategoryComponent)
+  },
+  {
+    path: 'product/:id',
+    loadComponent: () => import('./product-detail/product-detail.component').then(m => m.ProductDetailComponent)
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent)
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/components/product-box/product-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-box',
+  imports: [],
+  templateUrl: './product-box.component.html',
+  styleUrl: './product-box.component.css'
+})
+export class ProductBoxComponent {
+
+}
