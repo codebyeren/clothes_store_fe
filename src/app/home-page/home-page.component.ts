0eb68f0d7ec372fda62124e54b12380f52eb8294
Eren <www.viet712@gmail.com>
2025-05-16 18:58:37 +0700

    Update first

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ProductService, ProductCategory } from '../services/product.service';
+import { ProductBoxComponent } from '../components/product-box/product-box.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  standalone: true,
+  imports: [
+    ProductBoxComponent,
+    NgFor,
+    NgIf
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrls: ['./home-page.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  allCategories: ProductCategory[] = [];
+  visibleCategories: ProductCategory[] = [];
+  maxVisibleItems = 4;
+  step = 4;
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(response => {
+      this.allCategories = response.data;
+      this.updateVisibleCategories();
+    });
+  }
+
+  loadMore(): void {
+    this.maxVisibleItems += this.step;
+    this.updateVisibleCategories();
+  }
+
+  private updateVisibleCategories(): void {
+    this.visibleCategories = this.allCategories.slice(0, this.maxVisibleItems);
+  }
+
+  hasMore(): boolean {
+    return this.visibleCategories.length < this.allCategories.length;
+  }
 }
