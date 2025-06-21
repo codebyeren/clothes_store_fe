1c5e39889737a46f561d8ec9444ffecfb2a36103
Eren <www.viet712@gmail.com>
2025-06-21 17:11:24 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://clothing-store-api-8-1h2a.onrender.com//api',
-  wsUrl: 'https://clothing-store-api-8-1h2a.onrender.com//'
+  apiUrl: 'https://clothing-store-api-8-1h2a.onrender.com/api',
+  wsUrl: 'https://clothing-store-api-8-1h2a.onrender.com/'
 };
